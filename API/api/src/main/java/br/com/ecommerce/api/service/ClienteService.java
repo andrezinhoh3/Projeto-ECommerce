84750ba29d4e957bbd91195d3ec777a7c3435560
André <andre.jose_@hotmail.com>
2025-09-03 21:52:37 -0300

    atualizando

---
+++ API/api/src/main/java/br/com/ecommerce/api/service/ClienteService.java
@@ -0,0 +1,24 @@
+package br.com.ecommerce.api.service;
+
+import br.com.ecommerce.api.model.Cliente;
+import br.com.ecommerce.api.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
+public class ClienteService  {
+    // Injecao de Dependencia
+    // Falar que Service depende de alguem
+    // Final - constante
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository repo) {
+        this.clienteRepository = repo;
+    }
+
+    // Listar todos os clientes
+    public List<Cliente> listarTodos () {
+        return clienteRepository.findAll();
+    }
+}
