176bc55444fed1fd33200430864b6f6ea12b7997
André José <165748202+andrezinhoh3@users.noreply.github.com>
2025-09-17 23:38:51 -0300

    Delete API/entregas/src/main/java/br/com/projeto_senai/entregas/controller/UsuarioController.java
    deletando por erros

--- API/entregas/src/main/java/br/com/projeto_senai/entregas/controller/UsuarioController.java
+++
@@ -1,9 +0,0 @@
-package br.com.projeto_senai.entregas.controller;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-@RestController
-@RequestMapping("/api/usuarios")
-@Tag(name = "Usuarios", description = "Endpoints para gerenciamento de usuarios")
-public class UsuarioController {
-}
